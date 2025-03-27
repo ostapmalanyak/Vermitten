981232ef29432a1f8090a8039d48e9d93b844cc5
ostapmalanyak <o.malanyak@students.htl-leonding.ac.at>
2025-03-27 11:12:02 +0100

    Added a basic Card abstract class, 3d models for Cards and a basic mitten 3d model

---
+++ vermitten/Scripts/Card.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace Vermitten.Scripts;
+
+public abstract partial class Card : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
