942749187c37794f5e61dbf5cb37dd2ccedc27f4
ostapmalanyak <o.malanyak@students.htl-leonding.ac.at>
2025-04-10 09:32:30 +0200

    Made it so when hovering mouse over card, the card mesh moves
    2 constants define the move behaviour: how much it goes forward and in how many seconds
    I also added a scaling behaviour but it's not used for now at least

--- vermitten/Scripts/Card.cs
+++ vermitten/Scripts/Card.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System;
 using Godot;
 
@@ -5,42 +6,74 @@ namespace Vermitten.Scripts;
 
 public partial class Card : Node3D
 {
-	protected const float ShakeRotation = 15f;
-	protected const float ShakeSpeed = 10f;
+	private const float SelectScale = 0f;
+	private const float SelectMove = 5f;
+	private const float SelectSeconds = 0.05f;
 	
 	public bool MouseHovering = false;
-	protected float Rotated = 0;
-	protected float Mult = 1;
+	private int _selectingState = 0; // 0 - neutral, 1 - moving up, 2 - moving back
+	private float _selectingFrame = 0;
+	private float _rotated = 0;
+	private float _mult = -1;
+	private Vector3 _prevPos;
+	private MeshInstance3D? _cardMesh;
 	
-	public override void _Ready()
-	{
-		
+	public override void _Ready() {
+		_cardMesh = GetChild<MeshInstance3D>(0);
+		if (_cardMesh is null) throw new Exception($"No card mesh at {this}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (MouseHovering) {
-			if (Rotated < ShakeRotation) {
-				Rotate(new Vector3(0, 1, 0),  ToRadians(1)*Mult);
-				Rotated += 1;
+		if (_cardMesh is null) throw new Exception($"No card mesh at {this}");
+		
+		if (_selectingState == 1) {
+			if (_selectingFrame<SelectSeconds) {
+				_selectingFrame+=(float)delta;
+				
+				_cardMesh.Translate(Vector3.Up*(SelectMove/SelectSeconds)*(float)delta);
+				_cardMesh.Scale = new Vector3(
+					1f + SelectScale/SelectSeconds*_selectingFrame,
+					1f + SelectScale/SelectSeconds*_selectingFrame,
+					1f + SelectScale/SelectSeconds*_selectingFrame);
 			}
 			else {
-				Rotated *= Mult;
-				Mult *= -1;
+				_cardMesh.Position = new Vector3(_cardMesh.Position.X, _cardMesh.Position.Y, SelectMove*-1);
+				_cardMesh.Scale = new Vector3(1f + SelectScale, 1f + SelectScale, 1f + SelectScale);
+				
+				_selectingState = 0;
+			}
+		}
+		else if (_selectingState == 2) {
+			if (_selectingFrame>0) {
+				_selectingFrame-=(float)delta;
+				
+				_cardMesh.Translate(Vector3.Down*(SelectMove/SelectSeconds)*(float)delta);
+				_cardMesh.Scale = new Vector3(
+					1f + (SelectScale/SelectSeconds)*_selectingFrame,
+					1f + (SelectScale/SelectSeconds)*_selectingFrame,
+					1f + (SelectScale/SelectSeconds)*_selectingFrame);
+			}
+			else {
+				_cardMesh.Position = _prevPos;
+				_cardMesh.Scale = new Vector3(1f, 1f, 1f);
+				
+				_selectingState = 0;
 			}
 		}
 	}
 
-	public void StartShake() {
+	private void StartShake() {
+		if (_cardMesh is null) throw new Exception($"No card mesh at {this}");
 		MouseHovering = true;
+		_prevPos = _cardMesh.Position;
+		_selectingState = 1;
 	}
 
-	public void StopShaking() {
+	private void StopShaking() {
+		if (_cardMesh is null) throw new Exception($"No card mesh at {this}");
 		MouseHovering = false;
-	}
-
-	private float ToRadians(float degrees) {
-		return (float) (degrees / 180 * Math.PI);
+		_selectingState = 2;
 	}
 }
